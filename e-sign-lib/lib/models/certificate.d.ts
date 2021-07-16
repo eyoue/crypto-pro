37e89119d6a1da48b91a42f607067a214b1c8f8e
Maxim Zv <tobascoo@yandex.ru>
2021-07-16 12:49:00 +0300

    3

--- e-sign-lib/lib/models/certificate.d.ts
+++
@@ -1,9 +0,0 @@
-export interface CertificateModel {
-    issuerName: string;
-    isValid: boolean;
-    name: string;
-    thumbprint: string;
-    validFrom: string;
-    validTo: string;
-    class?: string;
-}
--- e-sign-lib/utp-e-sign-lib.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="@utp/e-sign-lib" />
-export * from './public-api';
